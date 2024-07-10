f8e93626a2ddc6bd8edc25b222d1d40175b6ec1d
playcsgo <sakura_0125@hotmail.com>
2024-07-10 09:52:58 +0800

    create index.html

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+window.location.replace("https://playcsgo.github.io/coffee_frontend/")
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
-  <div>
-    <meta http-equiv="refresh" content="0; url=https://playcsgo.github.io/coffee_frontend/"></meta>
-  </div>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+  // <div>
+  //   <meta http-equiv="refresh" content="0; url=https://playcsgo.github.io/coffee_frontend/"></meta>
+  // </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
